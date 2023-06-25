257604a6af8ea600635ceb2b4126f2180413dbb9
Collins Mmasaba <mmasabacollins9@gmail.com>
2023-06-25 11:26:38 +0300

    storing data in text files

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request
+from markupsafe import escape
 app = Flask(__name__)
 
 
 def search_for_letters(phrase: str, letters: str = 'aeiou') -> set:
     return set(phrase).intersection(set(letters))
+
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('search.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search', methods=['POST'])
@@ -12,13 +18,31 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results'
     results = str(search_for_letters(phrase, letters))
-    return render_template('results.html', the_title=title, the_phrase=phrase, the_letters=letters, the_results=results)
+    log_request(request, results)
+    return render_template('results.html', the_title=title, the_phrase=phrase,
+                           the_letters=letters, the_results=results)
 
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title='Welcome to search for letters on the web!')
+    return render_template('entry.html', the_title='Welcome to search '
+                                                   '4 letters on the web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('search.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote Address', 'User Agent', 'Results')
+    return render_template('viewlog.html',
+                           the_row_titles=titles,
+                           the_data=contents,
+                           the_title='View Search Log',)
 
 
 if __name__ == '__main__':
