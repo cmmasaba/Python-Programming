875361eecd91b8f5450ce48518f2b9aaead7193d
Collins Mmasaba <mmasabacollins9@gmail.com>
2023-08-17 07:20:54 +0300

    numeric operations

---
+++ operators.py
@@ -0,0 +1,15 @@
+# example of operators in python and there uses
+# Operators include:
+#   +   addition
+#   -   subtraction
+#   /   division
+#   %   modulus
+#   //  floor division
+#   **  exponent
+
+print('2 + 2:', 2+2)
+print('10 - 20:', 10-20)
+print('100 / 1.765:', 100/20) #  floating point support
+print('17 % 3:', 17%3)
+print('17 // 3:', 17//3)
+print('2**10:', 2**10)
